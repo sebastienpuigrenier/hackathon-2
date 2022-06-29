12806da91f3893c2ab2d5d9dfaaf06c9de47f28b
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-06-30 01:13:35 +0200

    hotfix

--- backend/src/models/ProjectsManager.js
+++ backend/src/models/ProjectsManager.js
@@ -24,23 +24,6 @@ class ProjectsManager extends AbstractManager {
       ]
     );
   }
-  /*
-  {
-    back 23:46:22   id: '624788dc-9d78-42a5-9427-ad68bed63dfe',    
-    back 23:46:22   firstname: 'Eugene',
-    back 23:46:22   lastname: 'Dostie',
-    back 23:46:22   email: 'eugenedostie@apside.com',
-    back 23:46:22   site: 'Apside Berlin',
-    back 23:46:22   status: 'idea',
-    back 23:46:22   creation_date: '2022-06-29',
-    back 23:46:22   belonging_site: 'Apside Berlin',
-    back 23:46:22   name: 'fsg',
-    back 23:46:22   customer: 'sfg',
-    back 23:46:22   languages: 'sfg',
-    back 23:46:22   keywords: 'sfg',
-    back 23:46:22   description: 'sfdg',
-    back 23:46:22   goals: 'sfg'
-    back 23:46:22 } */
 
   update(project, id) {
     return this.connection.query(
