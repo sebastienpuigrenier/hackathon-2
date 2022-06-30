f5ce898cddd6a2a57865f58f56c50c26ae591cc2
FREDERIQUE DEMAS <frederique.demas73@gmail.com>
2022-06-30 15:30:40 +0200

    adding link to statistics page

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -58,11 +58,11 @@ function Chart() {
   //   );
   //   console.log("numberOfFinished", numberOfFinished);
 
-//   const dataMock = [
-//     { status: "One", value: 10, color: "#E38627" },
-//     { title: "Two", value: 15, color: "#C13C37" },
-//     { title: "Three", value: 20, color: "#6A2135" },
-//   ];
+  //   const dataMock = [
+  //     { status: "One", value: 10, color: "#E38627" },
+  //     { title: "Two", value: 15, color: "#C13C37" },
+  //     { title: "Three", value: 20, color: "#6A2135" },
+  //   ];
 
   const defaultLabelStyle = {
     fontSize: "5px",
